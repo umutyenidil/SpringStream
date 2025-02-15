b6c3c93ee5e9791d4e7f2233bde82444896d2ec2
Umut Yenidil <umut.yenidil9@gmail.com>
2025-02-15 22:53:17 +0300

    video streaming chunk olarak saglandi

--- springstream/src/main/java/com/umutyenidil/springstream/controllers/VideoController.java
+++ springstream/src/main/java/com/umutyenidil/springstream/controllers/VideoController.java
@@ -1,22 +1,34 @@
 package com.umutyenidil.springstream.controllers;
 
+import ch.qos.logback.core.boolex.EvaluationException;
 import com.umutyenidil.springstream.entities.Video;
 import com.umutyenidil.springstream.payload.CustomMessage;
 import com.umutyenidil.springstream.services.VideoService;
 import com.umutyenidil.springstream.services.impl.VideoServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping(
         "/api/v1/videos"
@@ -80,9 +92,77 @@ public class VideoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Video>> getAll(){
+    public ResponseEntity<List<Video>> getAll() {
         return ResponseEntity
                 .ok()
                 .body(videoService.getAll());
+    }
+
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoRange(
+            @PathVariable String videoId,
+            @RequestHeader(value = "Range", required = false) String range
+    ) {
+        Video video = videoService.get(videoId);
+
+        String contentType = video.getContentType();
+        if (contentType == null) contentType = "application/octet-stream";
+
+        Path path = Paths.get(video.getFilePath());
+
+        Resource resource = new FileSystemResource(path);
+
+        long fileLength = path.toFile().length();
+
+        if (range == null) {
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        }
+
+        long rangeStart, rangeEnd;
+
+        String[] ranges = range.replace("bytes=", "").split("-");
+
+        rangeStart = Long.parseLong(ranges[0]);
+
+        if (range.length() > 1) {
+            rangeEnd = Long.parseLong(ranges[1]);
+        } else {
+            rangeEnd = fileLength - 1;
+        }
+
+        if (rangeEnd > fileLength - 1) {
+            rangeEnd = fileLength - 1;
+        }
+
+        InputStream inputStream;
+
+        try {
+            inputStream = Files.newInputStream(path);
+            inputStream.skip(rangeStart);
+
+        } catch (IOException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+
+        long contentLength = rangeEnd - rangeStart + 1;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Range", "bytes=" + rangeStart + "-" + rangeEnd + "/" + fileLength);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("X-Content-Type-Options", "nosniff");
+        headers.setContentLength(contentLength);
+
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .headers(headers)
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(new InputStreamResource(inputStream));
     }
 }
